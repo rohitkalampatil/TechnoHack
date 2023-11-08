02d0f88034c9161861c3e46ace4b162686d616b2
rohitkalampatil <rohitkalam96k@gmail.com>
2023-11-08 14:03:25 +0530

    NumberGuessing Game

--- NumberGuessingGame.java
+++ NumberGuessingGame.java
@@ -4,29 +4,36 @@ public class NumberGuessingGame {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
-
-        int min = 1;
-        int max = 100;
-        int randomNumber = random.nextInt(max - min + 1) + min;
+        //generate Random Number
+        int randomNumber = random.nextInt(100);
+        //counts Attempts
         int attempts = 0;
         int guess;
 
         System.out.println("Welcome to the Number Guessing Game!");
-        System.out.println("I'm thinking of a number between " + min + " and " + max + ". Try to guess it.");
+        System.out.println("I'm thinking of a number between " + 1 + " and " + 100 + ". Try to guess it in 10 Attempts.");
 
-        do {
+        //iterate for 10 attempts
+        for (int i=1; i<=10; i++ ) {
             System.out.print("Enter your guess: ");
             guess = scanner.nextInt();
             attempts++;
 
-            if (guess < randomNumber) {
+            if (guess == randomNumber){
+                System.out.println("Congratulations! You've guessed the correct number: " + randomNumber);
+                System.out.println("In " + attempts + " attempts.");
+                break;
+            }else if (guess < randomNumber) {
                 System.out.println("The number is higher. Try again.");
             } else if (guess > randomNumber) {
                 System.out.println("The number is lower. Try again.");
             }
-        } while (guess != randomNumber);
 
-        System.out.println("Congratulations! You've guessed the correct number: " + randomNumber);
-        System.out.println("In " + attempts + " attempts.");
+            if (i == 10){
+                System.out.println("You Exhausted 10 rounds");
+            }
+
+        }
+
     }
 }
